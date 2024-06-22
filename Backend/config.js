033c0937f7233a434f0e5544f1d7486e665defea
withtahmid <tahmid.rakib@northsouth.edu>
2024-06-22 21:23:16 +0600

    dot dot

--- Backend/config.js
+++ Backend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoURI: 'mongodb+srv://withtahmid:MONGOPASS@cluster0.fenp9sy.mongodb.net/?retryWrites=true&w=majority',
     mongoURI2: 'mongodb+srv://withtahmid:AAAbbbcccddd123@cluster0.l5a3gti.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp'
-  };
+};
   
